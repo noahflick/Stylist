a207dbfff18e5a829507363fc660b0d9ad81edf6
Noah Flick <noahflick@gmail.com>
2016-05-23 10:33:48 -0700

    localhost:3000 set up

---
+++ config/database.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose')
+var dbUri = process.env.MONGOLAB_URI ||
+            'mongodb://localhost/' + process.env.LOCAL_DB;
+
+if (!process.env.MONGOLAB_URI) {
+  // check that MongoD is running...
+  require('net').connect(27017, 'localhost').on('error', function() {
+    console.log("YOU MUST BOW BEFORE THE MONGOD FIRST, MORTAL!");
+    process.exit(0);
+  });
+}
---
+++ config/routes.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+
+
+
+router.get('*', function(req, res, next) {
+  res.sendFile('public/index.html');
+});
+
+module.exports = router;
