b3dd1fb3c3162ba5ddb29adb546819390a44e59b
Noah Flick <noahflick@gmail.com>
2016-05-25 14:48:20 -0700

    now with heroku

--- public/js/stylists/stylistsController.js
+++ public/js/stylists/stylistsController.js
@@ -9,7 +9,7 @@ function StylistsController(UserResource, $stateParams, authService){
   var vm = this;
   if (authService.isLoggedIn()) {
     vm.user = authService.loggedInUser();
-    console.log('stylistsController working - user: ' + vm.user)
+    console.log('stylistsController working - role: ' + vm.user.role)
   }
 
   vm.test = 'stylistsController working'
